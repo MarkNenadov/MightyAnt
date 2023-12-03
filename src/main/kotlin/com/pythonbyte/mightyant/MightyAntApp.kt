2fd5812c9b9ef03d73df731f90ecf179ddc79198
Mark Nenadov <marknenadov@gmail.com>
2023-12-02 19:13:46 -0500

    refactor code to break down complexity in app class

--- src/main/kotlin/com/pythonbyte/mightyant/MightyAntApp.kt
+++ src/main/kotlin/com/pythonbyte/mightyant/MightyAntApp.kt
@@ -1,82 +1,26 @@
 package com.pythonbyte.mightyant
 
 import com.pythonbyte.mightyant.config.MightyAntConfig
-import com.pythonbyte.mightyant.util.sendToWebsocket
-import org.http4k.server.Jetty
-import org.http4k.server.asServer
 import org.http4k.websocket.Websocket
 import org.http4k.websocket.WsMessage
-import org.pythonbyte.krux.json.JsonObject
-import org.pythonbyte.krux.string.asString
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.Constructor
-import java.io.ByteArrayOutputStream
-import java.util.Base64
-import java.util.zip.GZIPOutputStream
-import kotlin.text.Charsets.UTF_8
 
 fun main() {
-    val config = getConfig()
-
-    if (config != null) {
-        { proxySocket: Websocket ->
-            if (!config.silent) {
-                proxySocket.send(
-                    WsMessage("Welcome to ${config.proxyName} Proxy"),
-                )
-            }
-            proxySocket.onMessage {
-                val content = it.body.payload.asString()
-
-                if (config.arbitraryMode) {
-                    val jsonObject = JsonObject(content)
-
-                    sendToWebsocket(
-                        jsonObject.getString("url"),
-                        transformations(config, jsonObject.getString("content")),
-                        proxySocket,
-                    )
-                } else {
-                    sendToWebsocket(
-                        config.destinationUrl,
-                        transformations(config, content),
-                        proxySocket,
-                    )
-
-                    config.mirrorUrls.forEach { mirrorUrl: String ->
-                        sendToWebsocket(mirrorUrl, content, proxySocket)
-                    }
-                }
-            }
-        }.asServer(
-            Jetty(config.proxyPort),
-        ).start()
-    } else {
-        println("Unable to start Proxy, due to missing resources/config.yaml")
-    }
+    val proxyListener = WebSocketProxyRunner(getConfig())
+    proxyListener.run()
 }
 
-fun transformations(config: MightyAntConfig, content: String): String {
-    var newContent = content
-
-    if (config.useBase64) {
-        val byteArray = newContent.toByteArray()
-        newContent = Base64.getEncoder().encodeToString(byteArray)
-    }
-
-    if (config.useCompression) {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        GZIPOutputStream(byteArrayOutputStream).bufferedWriter(UTF_8).use { it.write(content) }
-        newContent = String(byteArrayOutputStream.toByteArray())
-    }
-
-    return newContent
+fun sendWelcomeMessage(proxySocket: Websocket, config: MightyAntConfig) {
+    proxySocket.send(
+        WsMessage("Welcome to ${config.proxyName} Proxy"),
+    )
 }
 
-fun getConfig(): MightyAntConfig? {
+fun getConfig(): MightyAntConfig {
     val inputStream = Thread.currentThread().contextClassLoader.getResourceAsStream("config.yaml")
     if (inputStream != null) {
         return Yaml(Constructor(MightyAntConfig::class.java)).load(inputStream) as MightyAntConfig
     }
-    return null
+    throw Exception("Can't load Mighty Ant config file (resources/config.yaml)")
 }
---
+++ src/main/kotlin/com/pythonbyte/mightyant/WebSocketProxyRunner.kt
@@ -0,0 +1,76 @@
+package com.pythonbyte.mightyant
+
+import com.pythonbyte.mightyant.config.MightyAntConfig
+import com.pythonbyte.mightyant.util.sendToWebsocket
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+import org.http4k.websocket.Websocket
+import org.pythonbyte.krux.json.JsonObject
+import org.pythonbyte.krux.string.asString
+import java.io.ByteArrayOutputStream
+import java.util.*
+import java.util.zip.GZIPOutputStream
+
+class WebSocketProxyRunner(val config: MightyAntConfig): Runnable {
+    private lateinit var proxySocket: Websocket
+
+    private fun processRegularMode(content: String) {
+        sendToWebsocket(
+            config.destinationUrl,
+            transformations(content),
+            proxySocket,
+        )
+
+        config.mirrorUrls.forEach { mirrorUrl: String ->
+            sendToWebsocket(mirrorUrl, content, proxySocket)
+        }
+    }
+
+    private fun processArbitraryMode(content: String) {
+        val jsonObject = JsonObject(content)
+
+        sendToWebsocket(
+            jsonObject.getString("url"),
+            transformations(jsonObject.getString("content")),
+            proxySocket,
+        )
+    }
+
+    private fun transformations(content: String): String {
+        var newContent = content
+
+        if (config.useBase64) {
+            val byteArray = newContent.toByteArray()
+            newContent = Base64.getEncoder().encodeToString(byteArray)
+        }
+
+        if (config.useCompression) {
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            GZIPOutputStream(byteArrayOutputStream).bufferedWriter(Charsets.UTF_8).use { it.write(content) }
+            newContent = String(byteArrayOutputStream.toByteArray())
+        }
+
+        return newContent
+    }
+
+    override fun run() {
+        {
+                proxySocket: Websocket ->
+            this.proxySocket = proxySocket
+            if (!config.silent) {
+                sendWelcomeMessage(proxySocket, config)
+            }
+            proxySocket.onMessage {
+                val content = it.body.payload.asString()
+
+                if (config.arbitraryMode) {
+                    processArbitraryMode(content)
+                } else {
+                    processRegularMode(content)
+                }
+            }
+        }.asServer(
+            Jetty(config.proxyPort),
+        ).start()
+    }
+}

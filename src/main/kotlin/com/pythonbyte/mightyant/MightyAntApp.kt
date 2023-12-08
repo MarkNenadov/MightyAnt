def6ec1337052d249c9e279c729f2c02b0d82375
Mark Nenadov <marknenadov@gmail.com>
2023-12-07 20:03:36 -0500

    bumnp pythonbyte-krux

--- src/main/kotlin/com/pythonbyte/mightyant/MightyAntApp.kt
+++ src/main/kotlin/com/pythonbyte/mightyant/MightyAntApp.kt
@@ -1,23 +1,22 @@
 package com.pythonbyte.mightyant
 
 import com.pythonbyte.mightyant.config.MightyAntConfig
+import org.pythonbyte.krux.files.resourceToInputStream
 import org.yaml.snakeyaml.Yaml
-import java.io.InputStream
 
 fun main() {
     val proxyListener = WebSocketProxyRunner(getConfig())
     proxyListener.run()
 }
 
-
 fun getConfig(): MightyAntConfig {
-    val inputStream: InputStream = Thread.currentThread().contextClassLoader.getResourceAsStream("config.yaml")
-        ?: throw Exception("Can't load Mighty Ant config file (resources/config.yaml)")
-
     val yaml = Yaml()
 
     try {
-        return yaml.loadAs(inputStream, MightyAntConfig::class.java) ?: MightyAntConfig()
+        return yaml.loadAs(
+            resourceToInputStream("config.yaml"),
+            MightyAntConfig::class.java,
+        ) ?: MightyAntConfig()
     } catch (e: Exception) {
         throw Exception("Error loading YAML configuration", e)
     }
